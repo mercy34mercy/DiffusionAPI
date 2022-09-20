b6fb260af77432ea2e336d86782eb03c1c003761
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-09-20 15:52:32 +0900

    完成

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 from flask import Flask, request, send_file
+import requests
 
 
 load_dotenv()
@@ -22,8 +23,29 @@ app = Flask(__name__)
 def generate():
     req = request.args
     propmt = req.get("prompt", "")
+
+    text = propmt
+    source_lang = 'JA'
+    target_lang = 'EN'
+
+    # パラメータの指定
+    params = {
+                'auth_key' : os.environ['APIKEY'],
+                'text' : text,
+                'source_lang' : source_lang, # 翻訳対象の言語
+                "target_lang": target_lang  # 翻訳後の言語
+            }
+
+    # リクエストを投げる
+    response = requests.post("https://api-free.deepl.com/v2/translate", data=params) # URIは有償版, 無償版で異なるため要注意
+    result = response.json()
+
+    print(result["translations"][0]["text"])
+
+
+
     answers = stability_api.generate(
-        prompt=propmt
+        prompt=result["translations"][0]["text"]
     )
     for resp in answers:
         for artifact in resp.artifacts:
